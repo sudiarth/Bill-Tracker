c1dc7203817c8a2153e2d005b6224f0c6e6a93f8
Kadek Sudiartha <sudieartha@gmail.com>
2018-06-05 12:11:54 +0800

    add redirect url

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
-from flask import Flask, flash, session, request, redirect, render_template
+from flask import Flask, flash, session, request, redirect, render_template, url_for
 from db.data_layer import get_all_bills, get_bill, create_bill, delete_bill, update_bill
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    bills = get_all_bills()
+    return render_template('index.html', all_bills = bills)
 
 @app.route('/add_bill', methods=['POST'])
 def add_bill():
     description = request.form['description']
     amount = request.form['amount']
     create_bill(amount, description)
-    bills = get_all_bills()
-    return render_template('index.html', all_bills = bills)
+    return redirect(url_for('index'))
 
 app.run(debug=True)
