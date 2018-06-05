60a29069a45881c4035678533ac2574547aa51e7
Kadek Sudiartha <sudieartha@gmail.com>
2018-06-05 12:01:39 +0800

    show all bills on page

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ def add_bill():
     description = request.form['description']
     amount = request.form['amount']
     create_bill(amount, description)
-    return render_template('index.html')
+    bills = get_all_bills()
+    return render_template('index.html', all_bills = bills)
 
 app.run(debug=True)
